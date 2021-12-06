f316b802dd169ad0fcd8298fe995d5799b08df2d
MikiKru <michal_kruczkowski@o2.pl>
2021-12-06 15:52:54 +0100

    ex 2.6

---
+++ d1/exercises/ex_2_6.py
@@ -0,0 +1,11 @@
+day = int(input("podaj dzień"))
+month = int(input("podaj miesiąc"))
+year = int(input("podaj rok"))
+
+z = year - 1 if month < 3 else year
+c = 0 if month < 3 else 2
+
+day_of_week_index = (int(23 * month / 9) + day + 4 + year + (z // 4) - (z // 100) + (z // 400) - c) % 7
+
+day_index_mapper = ['ND','PN','WT','SR',"CZ",'PT','SO']
+print(f"Indeks dnia tygodnia {day_of_week_index} - {day_index_mapper[day_of_week_index]}")
