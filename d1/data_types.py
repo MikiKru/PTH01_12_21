14917b6bce16241965265653fd44a104541d73ef
MikiKru <michal_kruczkowski@o2.pl>
2021-12-06 10:57:12 +0100

    d1 variables & types

---
+++ d1/data_types.py
@@ -0,0 +1,8 @@
+name = 'Michał'
+integer_number = 1
+float_number = 1.92
+print(type(name), type(integer_number), type(float_number))
+print(name.upper(), name.lower(), name + '2', name * 3)
+print(integer_number * 5, str(integer_number) * 5)
+print(int(float_number))                    # konwersja zawężająca
+print(float(integer_number))                # konwersja rozszerzająca
