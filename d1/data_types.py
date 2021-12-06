81a3cd8848924640c85760e0e56606dab608f971
MikiKru <michal_kruczkowski@o2.pl>
2021-12-06 11:26:09 +0100

    ex 2.1

--- d1/data_types.py
+++ d1/data_types.py
@@ -1,8 +1,12 @@
 name = 'Michał'
 integer_number = 1
+integer_number1 = 1
 float_number = 1.92
 print(type(name), type(integer_number), type(float_number))
 print(name.upper(), name.lower(), name + '2', name * 3)
 print(integer_number * 5, str(integer_number) * 5)
 print(int(float_number))                    # konwersja zawężająca
 print(float(integer_number))                # konwersja rozszerzająca
+print(hex(name.__hash__()))
+print(hex(id(integer_number)))
+print(hex(id(integer_number1)))
---
+++ d1/exercises/ex_2_1.py
@@ -0,0 +1,4 @@
+from random import randint, randrange
+
+print(randint(1,6))
+print(randrange(1,7))
