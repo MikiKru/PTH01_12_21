1c456809a22654d3a179e4763dcedf5738309140
MikiKru <michal_kruczkowski@o2.pl>
2021-12-07 14:46:50 +0100

    ex 3.8

---
+++ d2/exercises/ex_3_8.py
@@ -0,0 +1,3 @@
+months = ["styczeń","luty","marzec","kwiecień","maj","czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"]
+
+print("".join([month+", " for month in months[::3]]))
