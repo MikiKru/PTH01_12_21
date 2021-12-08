5b76d0182fa056849b0ecf5775f55e9a6e4f4cfb
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 11:53:50 +0100

    ex 4.1

--- d3/exercises/ex_4_1.py
+++ d3/exercises/ex_4_1.py
@@ -8,3 +8,10 @@ day_of_week_index = (int(23 * month / 9) + day + 4 + year + (z // 4) - (z // 100
 
 day_index_mapper = ('niedziela','poniedziałek','wtorek','środa',"czwartek",'piątek','sobota')
 print(f"Data: {day:02}.{month:02}.{year} - dzień tygodnia - {day_index_mapper[day_of_week_index]}")
+
+def day_index(date):
+    pass
+def day_name(day_index):
+    pass
+def get_day_name(date, sep='-'):            # format wprowadzania daty YYYY-MM-dd
+    pass
