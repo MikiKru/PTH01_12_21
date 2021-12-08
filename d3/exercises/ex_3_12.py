3a7332b2e2d07e31fd275bbb05fbf64133070fe0
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 10:18:38 +0100

    d3 ex 3.12

--- d3/exercises/ex_3_12.py
+++ d3/exercises/ex_3_12.py
@@ -1,4 +1,5 @@
 # record = dict(zip(["name", "last_name", "address", "gender", "age"] , ["Jan", "Kowalski", "Warszawa", True, 25]))
+import statistics
 
 record1 = {"name" : "Jan", "lastName" : "Kowalski", "address" : "Warszawa", "gender" : True, "age" : 25}
 record2 = {'name': 'Dawid','lastName': 'Michalczyk','address': 'Warszawa', 'gender': True, 'age': 25}
@@ -9,7 +10,13 @@ record5 = {"name" : "Maciej", "lastName" : "Urbanski", "address" : "Łódź", "g
 database_table = [record1, record2, record3, record4, record5]
 
 # 1. SELECT * FROM table WHERE gender = True and address = 'Warszawa'
+men_in_warsaw = [record for record in database_table if record["gender"] and record["address"] == 'Warszawa']
+print(men_in_warsaw)
 
 # 2. SELECT * FROM table WHERE age between 30 and 40
+people_with_age = [record for record in database_table if record["age"] >= 30 and record["age"] <= 40]
+print(people_with_age)
 
-# 3. SELECT avg(age) FROM table WHERE name like 'A%'
+# 3. SELECT avg(age) FROM table WHERE name like 'A%' and gender = False
+ages = [record["age"] for record in database_table if record["name"][0] == "A" and not record["gender"]]
+print("Średnia wieku",statistics.mean(ages))
