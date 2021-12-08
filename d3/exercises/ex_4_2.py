65546862aa3ed163b413e2cc38fff99322ca1500
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 12:57:58 +0100

    ex 4.2

--- d3/exercises/ex_4_2.py
+++ d3/exercises/ex_4_2.py
@@ -1,30 +1,20 @@
 import statistics
 
-record1 = {"name" : "Jan", "lastName" : "Kowalski", "address" : "Warszawa", "gender" : True, "age" : 25}
-record2 = {'name': 'Dawid','lastName': 'Michalczyk','address': 'Warszawa', 'gender': True, 'age': 25}
-record3 = {"name" : "Alicja", "lastName" : "Kot" , "address" : "Katowice" , "gender" : False, "age" : 38}
-record4 = {"name" : "Anna", "lastName": "Nowak", "address": "Szczecin", "gender" : False, "age" : 37}
-record5 = {"name" : "Maciej", "lastName" : "Urbanski", "address" : "Łódź", "gender" : True, "age" : 44}
+def get_data():
+    record1 = {"name": "Jan", "lastName": "Kowalski", "address": "Warszawa", "gender": True, "age": 25}
+    record2 = {'name': 'Dawid', 'lastName': 'Michalczyk', 'address': 'Warszawa', 'gender': True, 'age': 25}
+    record3 = {"name": "Alicja", "lastName": "Kot", "address": "Katowice", "gender": False, "age": 38}
+    record4 = {"name": "Anna", "lastName": "Nowak", "address": "Szczecin", "gender": False, "age": 37}
+    record5 = {"name": "Maciej", "lastName": "Urbanski", "address": "Łódź", "gender": True, "age": 44}
+    return [record1, record2, record3, record4, record5]
+def get_data_by_gender_and_address(data, gender=True, address='Warszawa'):
+    return [record for record in data if record["gender"] == gender and record["address"] == address]
+def get_data_by_age(data, low_tresh, high_tresh):
+    return [record for record in data if record["age"] >= low_tresh and record["age"] <= high_tresh]
+def get_mean_age(data, gender, name_starts_with='A'):
+    result = [record["age"] for record in data if record["name"][0] == name_starts_with and record["gender"] == gender]
+    return statistics.mean(result) if result else 0
 
-database_table = [record1, record2, record3, record4, record5]
-
-# 1. SELECT * FROM table WHERE gender = True and address = 'Warszawa'
-men_in_warsaw = [record for record in database_table if record["gender"] and record["address"] == 'Warszawa']
-print(len(men_in_warsaw), men_in_warsaw)
-
-# 2. SELECT * FROM table WHERE age between 30 and 40
-people_with_age = [record for record in database_table if record["age"] >= 30 and record["age"] <= 40]
-print(len(people_with_age), people_with_age)
-
-# 3. SELECT avg(age) FROM table WHERE name like 'A%' and gender = False
-ages = [record["age"] for record in database_table if record["name"][0] == "A" and not record["gender"]]
-print("Średnia wieku",statistics.mean(ages))
-
-def get_data():         # funkcja pobierająca dene z bazy danych
-    pass
-def get_data_by_gender_and_address(gender=True, address='Warszawa'):
-    pass
-def get_data_by_age(low_tresh, high_tresh):
-    pass
-def get_mean_age(gender, name_starts_with='A'):
-    pass
+print(get_data_by_gender_and_address(get_data()))
+print(get_data_by_age(get_data(), 20, 40))
+print(get_mean_age(get_data(),True))
