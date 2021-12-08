fda6f663e2acdd99decb7cbbf5e588cd0651d0b1
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 12:17:47 +0100

    ex 4.1

--- d3/exercises/ex_4_1.py
+++ d3/exercises/ex_4_1.py
@@ -1,17 +1,16 @@
-day = int(input("podaj dzień"))
-month = int(input("podaj miesiąc"))
-year = int(input("podaj rok"))
+def day_index(year, month, day):
+    z = year - 1 if month < 3 else year
+    c = 0 if month < 3 else 2
+    return (int(23 * month / 9) + day + 4 + year + (z // 4) - (z // 100) + (z // 400) - c) % 7
 
-z = year - 1 if month < 3 else year
-c = 0 if month < 3 else 2
-day_of_week_index = (int(23 * month / 9) + day + 4 + year + (z // 4) - (z // 100) + (z // 400) - c) % 7
-
-day_index_mapper = ('niedziela','poniedziałek','wtorek','środa',"czwartek",'piątek','sobota')
-print(f"Data: {day:02}.{month:02}.{year} - dzień tygodnia - {day_index_mapper[day_of_week_index]}")
-
-def day_index(date):
-    pass
 def day_name(day_index):
-    pass
+    return ('niedziela','poniedziałek','wtorek','środa',"czwartek",'piątek','sobota')[day_index]
+
 def get_day_name(date, sep='-'):            # format wprowadzania daty YYYY-MM-dd
-    pass
+    print(str(date).split(sep))
+    year, month, day = str(date).split(sep)
+    return int(year), int(month), int(day)
+
+year, month, day = get_day_name(input("podaj datę w formacie YYYY-MM-dd"))
+print(day_name(day_index(year, month, day)))
+
