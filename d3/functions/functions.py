f30decf25ec85ac5c56be32239172dd3d1859f87
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 11:46:13 +0100

    different number of params

--- d3/functions/functions.py
+++ d3/functions/functions.py
@@ -1,4 +1,5 @@
 from random import random
+from statistics import mean
 
 
 def hello_world():
@@ -23,3 +24,15 @@ def get_record(name, lastname, gender = True, age = 18):
     return dict(zip(
         ["name","lastname", "gender", "age"],
         [name,lastname, gender, age]))
+
+def get_avg_from_grades(*grades):
+    for index, grade in enumerate(grades):
+        print(index, grade)
+    return mean(grades)
+
+def get_avg_from_grades_named_params(**grades):
+    print(grades.keys())
+    print(grades.values())
+    for key, grade in grades.items():
+        print(key, grade)
+    return mean(grades.values())
--- d3/functions/main.py
+++ d3/functions/main.py
@@ -1,4 +1,5 @@
-from d3.functions.functions import hello_world, hello_me, get_random_value, division, filter_by_treshold, get_record
+from d3.functions.functions import hello_world, hello_me, get_random_value, division, filter_by_treshold, get_record, \
+    get_avg_from_grades, get_avg_from_grades_named_params
 
 hello_world()
 hello_me("Michał")
@@ -16,5 +17,13 @@ print(filter_by_treshold([.2,.1,.3,.2,.4,.5,.4,.3], get_random_value()))
 # wywołania funkcji z parametrami domyślnymi
 print(get_record("Adam", "Kowalski", age=30))
 print(get_record("Anna", "Kowalska", age=29, gender=False))
+
+# Parametry nienazwane - dowolna ilość
+print("Średnia ocen:",get_avg_from_grades(3,4.5,3,5,4))
+print("Średnia ocen:",get_avg_from_grades(3,5,4))
+
+# PArametry nazwane - dowolna ilość
+print("Średnia ocen:",get_avg_from_grades_named_params(grade1=3,grade2=5,grade3=4))
+print("Średnia ocen:",get_avg_from_grades_named_params(grade1=3.5,grade2=5))
 
 
