f6ccaf21cfecc2470423e7d9504d591966f70b7e
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 12:44:43 +0100

    ex 4.2

---
+++ d3/exercises/ex_4_2.py
@@ -0,0 +1,30 @@
+import statistics
+
+record1 = {"name" : "Jan", "lastName" : "Kowalski", "address" : "Warszawa", "gender" : True, "age" : 25}
+record2 = {'name': 'Dawid','lastName': 'Michalczyk','address': 'Warszawa', 'gender': True, 'age': 25}
+record3 = {"name" : "Alicja", "lastName" : "Kot" , "address" : "Katowice" , "gender" : False, "age" : 38}
+record4 = {"name" : "Anna", "lastName": "Nowak", "address": "Szczecin", "gender" : False, "age" : 37}
+record5 = {"name" : "Maciej", "lastName" : "Urbanski", "address" : "Łódź", "gender" : True, "age" : 44}
+
+database_table = [record1, record2, record3, record4, record5]
+
+# 1. SELECT * FROM table WHERE gender = True and address = 'Warszawa'
+men_in_warsaw = [record for record in database_table if record["gender"] and record["address"] == 'Warszawa']
+print(len(men_in_warsaw), men_in_warsaw)
+
+# 2. SELECT * FROM table WHERE age between 30 and 40
+people_with_age = [record for record in database_table if record["age"] >= 30 and record["age"] <= 40]
+print(len(people_with_age), people_with_age)
+
+# 3. SELECT avg(age) FROM table WHERE name like 'A%' and gender = False
+ages = [record["age"] for record in database_table if record["name"][0] == "A" and not record["gender"]]
+print("Średnia wieku",statistics.mean(ages))
+
+def get_data():         # funkcja pobierająca dene z bazy danych
+    pass
+def get_data_by_gender_and_address(gender=True, address='Warszawa'):
+    pass
+def get_data_by_age(low_tresh, high_tresh):
+    pass
+def get_mean_age(gender, name_starts_with='A'):
+    pass
