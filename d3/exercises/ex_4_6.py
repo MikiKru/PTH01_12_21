7e42818605e89bd4f2b434907606b670089ef289
MikiKru <michal_kruczkowski@o2.pl>
2021-12-08 15:56:30 +0100

    ex_4_6

--- d3/exercises/ex_4_6.py
+++ d3/exercises/ex_4_6.py
@@ -1,15 +1,18 @@
-def day_index(year, month, day):
+def day_index(year : int, month : int, day : int) -> int:
     z = year - 1 if month < 3 else year
     c = 0 if month < 3 else 2
     return (int(23 * month / 9) + day + 4 + year + (z // 4) - (z // 100) + (z // 400) - c) % 7
 
-def day_name(day_index):
+def day_name(day_index : int) -> str:
     return ('niedziela','poniedziałek','wtorek','środa',"czwartek",'piątek','sobota')[day_index]
 
-def get_day_name(date, sep='-'):            # format wprowadzania daty YYYY-MM-dd
+def get_day_name(date : str, sep : str ='-') -> tuple:
     print(str(date).split(sep))
     year, month, day = str(date).split(sep)
     return int(year), int(month), int(day)
 
-year, month, day = get_day_name(input("podaj datę w formacie YYYY-MM-dd"))
-print(day_name(day_index(year, month, day)))
+#test
+year, month, day = get_day_name("2020-03-03")
+print(day_index(year, month, day))
+print(day_name(1))
+
