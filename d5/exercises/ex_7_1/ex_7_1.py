9e280a7dcfea02b1f3158480f388bc2e671e28ef
MikiKru <michal_kruczkowski@o2.pl>
2021-12-10 11:01:41 +0100

    ex 7.1

--- d5/exercises/ex_7_1/ex_7_1.py
+++ d5/exercises/ex_7_1/ex_7_1.py
@@ -7,6 +7,25 @@ with open(r'C:\Users\Student\PycharmProjects\PYTH01_12_21\d5\exercises\ex_7_1\fi
     for line in file2:
         print(line.strip())
 
-with open("../files/w2.txt", mode="r", encoding='utf-8') as file2:
-    for line in file2:
-        print(line.strip())
+# with open("../files/w2.txt", mode="r", encoding='utf-8') as file2:
+#     for line in file2:
+#         print(line.strip())
+
+# wypisz linie naprzemian z obu plików
+# file = open("w1.txt")
+# #...
+# file.close()
+
+with open(r'C:\Users\Student\PycharmProjects\PYTH01_12_21\d5\exercises\ex_7_1\files\w1.txt', mode="r", encoding='utf-8') as file1, \
+        open(r'C:\Users\Student\PycharmProjects\PYTH01_12_21\d5\exercises\ex_7_1\files\w2.txt', mode="r", encoding='utf-8') as file2:
+    file1_lines = file1.readlines()
+    file2_lines = file2.readlines()
+longer = file1_lines
+smaller = file2_lines
+if len(file1_lines) < len(file2_lines):
+    longer = file2_lines
+    smaller = file1_lines
+for index, line in enumerate(longer):
+    print(line.strip())
+    if index < len(smaller):
+        print(smaller[index].strip())
